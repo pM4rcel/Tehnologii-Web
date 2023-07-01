b43b6dc021bb4e31a9f63094f769ad5746811c97
Marcel <timofte.2002@gmail.com>
2023-07-01 09:13:41 +0300

    author + book + browse pages

--- src/static/scripts/utils.js
+++
@@ -1,7 +0,0 @@
-export const getCookie = (name) => {
-    for(let str in document.cookie.split(";")){
-        if(str.trim().startsWith(name + '='))
-            return true;
-    }
-    return false;
-}
